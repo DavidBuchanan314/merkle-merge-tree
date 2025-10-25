1309620210bda0958720b7bbb29743aad41b63e3
David Buchanan <d@vidbuchanan.co.uk>
2025-10-25 22:58:39 +0100

    initial tree structure - no proof generation

---
+++ main.py
@@ -0,0 +1,182 @@
+r"""
+
+ok. ordered binary tree.
+
+when traversing the tree from the top, you need to know which fork to follow. how do we encode this?
+
+
+
+          4
+         / \
+        /   \
+       /     \
+      /       \
+     /         \
+    2           6  <- "rightmost value of the left subtree"
+   / \         / \
+  /   \       /   \
+ 1     3     5     7
+/ \   / \   / \   / \
+1 2   3 4   5 6   7 8
+
+
+Alternatively, you could binary-search the leaves (they are stored linearly - could use regular DB index to accelerate)
+
+If you know a leaf's index, you can immediately know its path.
+
+
+          d
+         / \
+        /   \
+       /     \
+      /       \
+     /         \
+    b           f
+   / \         / \
+  /   \       /   \
+ a     c     e     g
+/ \   / \   / \   / \
+1 2   3 4   5 6   7 8
+
+storage order:
+12a34cb56e78gfd
+(this order means a tree-merge operation uses linear IO)
+
+(length = 15) = 2^4-1
+
+inclusion proof of "3" would be: 4,a,f
+*ex*clusion proof of "3.5" would be inclusion proof of both 3 and 4
+
+
+A "Forest" is a tuple of trees, ordered from tallest to shortest.
+You can add a tree to the forest. If the two trees on the end of the list would be the same size, they are merged.
+Repeat until no more merges are possible. (A forest is only considered valid if it is in "minimal" form, per this process - I may relax this to reduce latency, with deferred merges)
+
+The root of the Forest is the hash of the concatenated tree roots.
+
+An inclusion proof for a Forest is an inclusion proof for one of its constituent trees, plus the other hashes you need to compute the root hash.
+
+An *exclusion* proof for a forest is a tuple of exclusion proofs for each tree.
+
+
+The log is a sequence of Forest roots.
+
+It would be expensive to check if a new entry was already in the tree, so we implement a "multiset" rather than a classic set - i.e. the same item can be added more than once. But inclusion proofs stop after the first found match.
+
+"""
+
+import io
+import heapq # used for heapq.merge
+from typing import Optional, Iterable
+import hashlib
+
+def count_trailing_ones(n: int) -> int:
+	return (n ^ (n + 1)).bit_length() - 1
+
+class Tree:
+	"""
+	the minimal tree has height one and contains a single leaf
+
+	trees are immutable
+	"""
+
+	def __init__(self, data: bytes) -> None:
+		assert(len(data) % 32 == 0)
+		num_hashes, rem = divmod(len(data), 32)
+		if rem != 0:
+			raise ValueError("invalid data length (not a multiple of 32 bytes)")
+		self.height = num_hashes.bit_length()
+		if num_hashes == 0 or num_hashes != (2**self.height)-1:
+			raise ValueError("invalid data length")
+		self.cardinality = 2**(self.height-1) # number of leaves
+		self.data = data
+		self.root = data[-32:]
+
+	def __repr__(self) -> str:
+		return f"Tree<height={self.height}, root={self.root.hex()}>"
+
+	def __iter__(self):
+		"""
+		iterate thru leaves
+		"""
+		offset = 0
+		for i in range(self.cardinality):
+			yield self.data[offset:offset+32]
+			offset += 32 + count_trailing_ones(i) * 32 # skip non-leaf entries
+
+	def merge(self, other) -> "Tree":
+		"""
+		merge two trees of equal height n to produce a new tree of height n+1
+		"""
+		if not isinstance(other, Tree):
+			raise TypeError("can only merge subtrees")
+		if self.height != other.height:
+			raise ValueError("can only merge trees of the same height")
+		data = io.BytesIO()
+		stack = []
+		for i, entry in enumerate(heapq.merge(self, other)):
+			data.write(entry)
+			stack.append(entry)
+			for _ in range(count_trailing_ones(i)):
+				# pull two, hash them, emit and push
+				b, a = stack.pop(), stack.pop()
+				h = hashlib.sha256(a+b).digest()
+				data.write(h)
+				stack.append(h)
+		return Tree(data.getvalue())
+
+	def __or__(self, other) -> "Tree":
+		return self.merge(other)
+
+
+class Forest:
+	"""
+	forests are immutable.
+	"add" operation produces a new tree.
+	"""
+	def __init__(self, trees: Optional[Iterable[Tree]]) -> None:
+		self.trees = () if trees is None else tuple(trees)
+		h = hashlib.sha256()
+		prev_height = float("Inf")
+		cardinality = 0
+		for tree in self.trees:
+			if tree.height >= prev_height:
+				raise ValueError("non-canonical tree order")
+			h.update(tree.root)
+			prev_height = tree.height
+			cardinality += tree.cardinality
+		self.root = h.digest()
+		self.cardinality = cardinality
+	
+	def get_roots(self) -> Iterable:
+		raise NotImplementedError("TODO")
+
+	def add(self, entry: bytes) -> "Forest":
+		if len(entry) != 32:
+			raise ValueError("entry must be 32 bytes long (expects sha256 hash output)")
+		accumulator = Tree(entry) # tree of height 1
+		i = 1
+		while i <= len(self.trees):
+			if self.trees[-i].height == accumulator.height:
+				accumulator |= self.trees[-i]
+			else:
+				break
+			i += 1
+		return Forest(self.trees[:len(self.trees)-(i-1)] + (accumulator,))
+
+
+if __name__ == "__main__":
+	a = Tree(b"A"*32)
+	b = Tree(b"B"*32)
+	c = Tree(b"C"*32)
+	d = Tree(b"D"*32)
+	x = a|b
+	y = c|d
+	z = x|y
+	print(list(enumerate(z)))
+	print(z.cardinality)
+	forest = Forest((z,))
+	print(forest.cardinality, forest.root.hex())
+
+	forest = forest.add(b"E"*32)
+	print(forest.trees)
